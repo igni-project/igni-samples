8ebc726b831e742044f5b62d6b04b3027ed94dfe
igni-project <igni.project@gmail.com>
2025-05-10 22:41:00 +1000

    further commentary on load_model.c

--- src/load_model.c
+++ src/load_model.c
@@ -8,11 +8,25 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+/* SUP requires all meshes have a unique identification code. */
+#define MESH_ID 3
+
+/* Sample: Load Model
+ * Difficulty: Easy */
 int load_model()
 {
+	/* Igni display socket */
+
+	/* Socket descriptor */
 	int fd;
+
+	/* Address */
 	struct sockaddr_un svAddr;
+
+	/* Path to socket */
 	const char* domain;
+
+	/* ------------------- */
 
 	printf("Running sample: Load Model\n");
 
@@ -33,10 +47,12 @@ int load_model()
 		return -1;
 	}
 
-	/**/
+	/* A sockaddr_un struct points to the socket to connect to. In this case,
+	 * the socket of path IGNI_DISPLAY. */
 	svAddr.sun_family = AF_UNIX;
 	strncpy(svAddr.sun_path, domain, sizeof(svAddr.sun_path));
 
+	/* The client may now connect to the server. */
 	if (connect(fd, (struct sockaddr*)&svAddr, sizeof(svAddr)) == -1)
 	{
 		perror("Failed to connect to server");
@@ -44,10 +60,10 @@ int load_model()
 	}
 
 	/* Import a mesh into the scene */
-	sup_asset_load(fd, 3, "/usr/share/igni-samples/mesh.obj");
+	sup_asset_load(fd, MESH_ID, "/usr/share/igni-samples/mesh.obj");
 
 	/* Make the imported mesh visible to viewers */
-	sup_asset_show(fd, 3);
+	sup_asset_show(fd, MESH_ID);
 
 	/* Halt for 60 seconds and quit */
 	sleep(60);
